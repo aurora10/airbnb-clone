aac1a1bfc9bde10c67e4016ddf97b0498fc7898d
aurora10 <aurora10@gmail.com>
2022-09-30 18:47:28 +0200

    mw

--- components/LagreCard.js
+++ components/LagreCard.js
@@ -4,7 +4,8 @@ import Image from "next/image";
 function LagreCard({ img, title, description, buttonText }) {
   return (
     <section className="relative py-16 cursor-pointer ">
-      <div className="relative h-96 min-w-fit">
+      <div className="relative h-96 min-w-[1320px]">
+        {/*  min-w-[1320px]*/}
         <Image
           src={img}
           layout="fill"
